2e6e6ea1f067241c7607abb7d4d3420163676dc1
Thomas Watson Steen <w@tson.dk>
2014-08-23 17:07:27 +0200

    Ensure tests quits even if there is stuff on the event loop

--- test.js
+++ test.js
@@ -46,3 +46,8 @@ require('abstract-leveldown/abstract/iterator-test').args(test)
 require('abstract-leveldown/abstract/iterator-test').sequence(test)
 require('abstract-leveldown/abstract/iterator-test').iterator(factory, test, testCommon, testCommon.collectEntries)
 require('abstract-leveldown/abstract/iterator-test').tearDown(test, testCommon)
+
+test('end', function (t) {
+  t.end();
+  process.nextTick(process.exit);
+})
